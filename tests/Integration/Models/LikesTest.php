765864c80666390b0a438c92c2f73d50de37d31e
Igor Kulakov <kulakov.igor@mail.ru>
2020-01-14 00:35:55 +0300

    Episode 9: "Liking" a Model With TDD

---
+++ tests/Integration/Models/LikesTest.php
@@ -0,0 +1,81 @@
+<?php
+
+
+namespace Tests\Integration\Models;
+
+use App\Post;
+use App\User;
+use Tests\TestCase;
+
+class LikesTest extends TestCase
+{
+
+    /** @test */
+    public function a_user_can_like_a_post()
+    {
+        $post = factory(Post::class)->create();
+        $user = factory(User::class)->create();
+
+        $this->actingAs($user);
+
+        $post->like();
+
+        $this->seeInDatabase('likes', [
+            'user_id' => $user->id,
+            'likeable_id' => $post->id,
+            'likeable_type' => get_class($post),
+        ]);
+
+        $this->assertTrue($post->isLiked());
+    }
+
+    /** @test */
+    public function a_user_can_unlike_a_post()
+    {
+        $post = factory(Post::class)->create();
+        $user = factory(User::class)->create();
+
+        $this->actingAs($user);
+
+        $post->like();
+        $post->unlike();
+
+        $this->notSeeInDatabase('likes', [
+            'user_id' => $user->id,
+            'likeable_id' => $post->id,
+            'likeable_type' => get_class($post),
+        ]);
+
+        $this->assertFalse($post->isLiked());
+    }
+
+    /** @test */
+    public function a_user_may_toggle_a_posts_like_status()
+    {
+        $post = factory(Post::class)->create();
+        $user = factory(User::class)->create();
+
+        $this->actingAs($user);
+
+        $post->toggle();
+
+        $this->assertTrue($post->isLiked());
+
+        $post->toggle();
+
+        $this->assertFalse($post->isLiked());
+    }
+
+    /** @test */
+    public function a_post_knows_how_many_likes_it_has()
+    {
+        $post = factory(Post::class)->create();
+        $user = factory(User::class)->create();
+
+        $this->actingAs($user);
+
+        $post->toggle();
+
+        $this->assertEquals(1, $post->likesCount);
+    }
+}
